8667bc1ca4725f05e4ae3c8bf79721fc4c080252
proudeng <jun_deng@outlook.com>
2016-11-14 21:46:01 +0800

    add pi_orin.c

---
+++ pi_orin.c
@@ -0,0 +1,16 @@
+static long num_rects = 1000000000000;
+
+void main()
+{
+	int i;
+	double mid, height, width, sum = 0.0;
+	double area;
+	width = 1.0/(double) num_rects;
+	for (i = 0; i < num_rects; i++){
+		mid = (i + 0.5) * width;
+		height = 4.0/(1.0 + mid*mid);
+		sum += height;
+	}
+	area = width * sum;
+	printf("Computed pi = %f\n", area);
+}
